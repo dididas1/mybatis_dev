a0d1f8fc60310a9de197f350e3060fc843f3f228
lsw <dididas@naver.com>
2017-03-14 10:12:11 +0900

    결과맵 매핑을 제외한 나머지 삭제

--- src/main/java/kr/or/digt/mybatis_dev/dao/StudentMapper.java
+++ src/main/java/kr/or/digt/mybatis_dev/dao/StudentMapper.java
@@ -9,28 +9,6 @@ import org.apache.ibatis.annotations.Update;
 import kr.or.dgit.mybatis_dev.dto.Student;
 
 public interface StudentMapper {
-	Student selectStudentByNo (Student student);
-	Student selectStudentByNoApi (Student student);
-	@Select("select stud_id,name,email,phone ,dob from students where stud_id = #{studId}")
-	Student selectStudnetByNoAnnotation(Student studnet);
-	
-	List<Student> selecStudentByAll();
-	List<Student> selecStudentByApi();
-	@Select("select stud_id,name,email,phone ,dob from students")
-	List<Student> selecStudentByAnnotation();
-	
-	int updateStudent(Student student);
-	int updateStudentApi(Student student);
-	@Update ("update students set name=#{name},email=#{email},phone=#{phone},dob=#{dob} where stud_id = #{studId}")
-	int updateStudentAnnotation(Student student);
-	
-	int insertStudent(Student student);
-	int insertStudentApi(Student student);
-	@Insert ("insert into students(stud_id,name,email,dob,phone) values(#{studId},#{name},#{email},#{dob},#{phone})")
-	int insertStudentAnnotation(Student student);
-	
-	int deleteStudent(Student student);
-	int deleteStudentApi(Student student);
-	@Insert ("delete from students where stud_id=#{studId}")
-	int deleteStudentAnnotation(Student student);
-}
+	Student selectStudentByNoForResultMap(Student studnet);
+	List<Student> selectStudentByAllForResultMap();
+	}
--- src/test/java/kr/or/dgit/mybatis_dev/StudentServiceTest.java
+++ src/test/java/kr/or/dgit/mybatis_dev/StudentServiceTest.java
@@ -1,6 +1,7 @@
 package kr.or.dgit.mybatis_dev;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -34,116 +35,18 @@ public class StudentServiceTest {
 		service = null;
 	}
 	
-	@Before
-	public void setUp(){
-		System.out.println("setUp()");
-	}
-	@After
-	public void tearDown(){
-		System.out.println("tearDown()");
-	}
-	
-/*	@Test
-	public void testselectStudentByNo() {
+	@Test
+	public void testselectStudentByNoForResultMap(){
 		Student std= new Student();
-		std.setStudId(1);
+		std.setStudId(2);
+		Student selectstd = service.selectStudentByNoForResultMap(std);
+		Assert.assertNotNull(selectstd);
 		
-		Student findstd= service.findStudentByNo(std);
-		Assert.assertNotNull(findstd);
-	}
-	
-	@Test
-	public void testselectStudentByNoApi() {
-		Student std= new Student();
-		std.setStudId(1);
-		Student findstd= service.findStudentByNoApi(std);
-	}
-	
-	@Test
-	public void testselectStudnetByNoAnnotation() {
-		Student std= new Student();
-		std.setStudId(1);
-		Student findstd= service.selectStudnetByNoAnnotation(std);
-	}
-	*/
-/*	@Test
-	public void testselecStudentByAll(){
-		List<Student> list= new ArrayList<>();
-		list = service.selecStudentByAll();
-		for(Student e:list){
-			System.out.println(e);
-		}
-		
-	}*/
-	/*@Test
-	public void testselecStudentByApi(){
-		List<Student> list= new ArrayList<>();
-		list = service.selecStudentByApi();
-		for(Student e:list){
-			System.out.println(e);
-		}
-		
-	}*/
-	
-/*	@Test
-	public void testselecStudentAnnotation(){
-		List<Student> list= new ArrayList<>();
-		list = service.selecStudentByAnnotation();
-		for(Student e:list){
-			System.out.println(e);
-		}
-	
-	}
-	*/
-	/*@Test
-	public void testupdateStudent(){
-		Student std = new Student(1, "kk", "kk@kk.net", new Date(), new PhoneNumber("010", "4040","3981" ));
-		service.updateStudent(std);
-		}*/
-	/*@Test
-	public void testupdateStudentAip(){
-		Student std = new Student(1, "cc", "kk@kk.net", new Date(), new PhoneNumber("010", "4040","3981" ));
-		service.updateStudentApi(std);
-	}*/
-	/*@Test
-	public void testupdateStudentAnnotation(){
-		Student std = new Student(1, "bb", "kk@kk.net", new Date(), new PhoneNumber("010", "4040","3981" ));
-		service.updateStudentAnnotation(std);
-	}*/
-	
-	/*@Test
-	public void testinsertStudent(){
-		Student std = new Student(3, "bb", "kk@kk.net", new Date(), new PhoneNumber("010", "4040","3981" ));
-		service.insertStudent(std);
-	}*/
-	/*@Test
-	public void testinsertStudentApi(){
-		Student std = new Student(4, "ee", "kk@kk.net", new Date(), new PhoneNumber("010", "4040","3981" ));
-		service.insertStudentApi(std);
-	}*/
-	
-	/*@Test
-	public void testinsertStudentAnnotation(){
-		Student std = new Student(5, "ff", "kk@kk.net", new Date(), new PhoneNumber("010", "4040","3981" ));
-		service.insertStudentAnnotation(std);
-	}*/
-	
-/*	@Test
-	public void testinsertStudent(){
-		Student std = new Student();
-		std.setStudId(5);
-		service.deleteStudent(std);
-	}*/
-	@Test
-	public void testinsertStudentApi(){
-		Student std = new Student();
-		std.setStudId(4);
-		service.deleteStudentApi(std);
 	}
 	@Test
-	public void testinsertStudentAnnotation(){
-		Student std = new Student();
-		std.setStudId(3);
-		service.deleteStudentAnnotation(std);
+	public void testselectStudentByAllForResultMap(){
+		List<Student> list = service.selectStudentByAllForResultMap();
+		List<Student> emplist = Collections.emptyList();
+		Assert.assertNotEquals(emplist, list);
 	}
 }
